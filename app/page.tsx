2134d5b20b39c0d2608ac01689e5147b59e82c6b
yojanFelix <johanalbertoqf169@gmail.com>
2025-11-07 21:04:05 -0700

    Se agregan y acomodan las imagenes

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 import Image from "next/image";
 import { Orbitron } from 'next/font/google';
-import logo_nvidia from "../public/images/logo_nvidia.png";
 import amd from "../public/Images/amd.png";
+import intel_logo from "../public/Images/intel_logo.png";
+import logo_nvidia from "../public/Images/logo_nvidia.png";
+import rog from "../public/Images/rog.png";
+import msi from "../public/Images/msi.png";
+import predator from "../public/Images/predator.png";
 const orbitron = Orbitron({ subsets: ['latin'], weight: ['700'] });
 
 export default function Home() {
@@ -51,12 +55,12 @@ export default function Home() {
       <Image
         src={logo_nvidia} 
         alt="Logo Nvidia"
-        width={150}
-        height={1500}
+        width={100}
+        height={100}
         style={{
           position: "absolute",
-          bottom: "0px",
-          right: "1000px",
+          bottom: "5px",
+          right: "90px",
         }}
       />
       <Image
@@ -67,7 +71,53 @@ export default function Home() {
         style={{
           position: "absolute",
           bottom: "20px",
-          right: "100px",
+          right: "1100px",
+        }}
+      />
+      <Image
+        src={intel_logo} 
+        alt="Logo intel"
+        width={70}
+        height={70}
+        style={{
+          position: "absolute",
+          bottom: "30px",
+          right: "1300px",
+        }}
+      />
+      <Image
+        src={rog} 
+        alt="Logo rog"
+        width={110}
+        height={110}
+        style={{
+          position: "absolute",
+          bottom: "0px",
+          right: "350px",
+          filter: "invert(1)",
+        }}
+      />
+      <Image
+        src={msi} 
+        alt="Logo msi"
+        width={110}
+        height={110}
+        style={{
+          position: "absolute",
+          bottom: "21px",
+          right: "215px",
+        }}
+      />
+      <Image
+        src={predator} 
+        alt="Logo acerpredator"
+        width={50}
+        height={50}
+        style={{
+          position: "absolute",
+          bottom: "21px",
+          right: "1200px",
+          filter: "invert(1)",
         }}
       />
     </div>
